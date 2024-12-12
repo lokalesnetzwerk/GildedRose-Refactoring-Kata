b4f59dcf37a825024c747d3fbca2b68f9fc752ea
Max <max.joel.mj@googlemail.com>
2024-12-12 19:40:04 +0100

    fix copy error

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,179 +5,152 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class GildedRoseTest {
 
-   package com.gildedrose;
+    Item[] items;
+    GildedRose testee;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+    @Test
+    void increaseQualityOfAgendBrie_WhenSellInNotNegative() {
+        items = new Item[]{new Item("Aged Brie", 1, 30)};
+        testee = new GildedRose(items);
 
-import java.util.Arrays;
-import java.util.Optional;
+        testee.updateQuality();
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Aged Brie")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(31);
+    }
 
-    class GildedRoseTest {
+    @Test
+    void increaseQualityOfAgendBrie_WhenSellInNegative() {
+        items = new Item[]{new Item("Aged Brie", -1, 30)};
+        testee = new GildedRose(items);
 
-        Item[] items;
-        GildedRose testee;
+        testee.updateQuality();
 
-        @Test
-        void increaseQualityOfAgendBrie_WhenSellInNotNegative() {
-            items = new Item[] {new Item("Aged Brie", 1, 30)};
-            testee = new GildedRose(items);
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Aged Brie")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(32);
+    }
 
-            testee.updateQuality();
+    @Test
+    void increaseQualityOfTickets_WhenSellInPositiveAndGreater10() {
+        items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 11, 30)};
+        testee = new GildedRose(items);
 
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Aged Brie")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(31);
-        }
+        testee.updateQuality();
 
-        @Test
-        void increaseQualityOfAgendBrie_WhenSellInNegative() {
-            items = new Item[] {new Item("Aged Brie", -1, 30)};
-            testee = new GildedRose(items);
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(10);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(31);
+    }
 
-            testee.updateQuality();
+    @Test
+    void increaseQualityOfTickets_WhenSellInPositiveAndGreater5() {
+        items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 6, 30)};
+        testee = new GildedRose(items);
 
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Aged Brie")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(32);
-        }
+        testee.updateQuality();
 
-        @Test
-        void increaseQualityOfTickets_WhenSellInPositiveAndGreater10() {
-            items = new Item[] {new Item("Backstage passes to a TAFKAL80ETC concert", 11, 30)};
-            testee = new GildedRose(items);
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(5);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(32);
+    }
 
-            testee.updateQuality();
+    @Test
+    void increaseQualityOfTickets_WhenSellInPositive() {
+        items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 1, 30)};
+        testee = new GildedRose(items);
 
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(10);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(31);
-        }
+        testee.updateQuality();
 
-        @Test
-        void increaseQualityOfTickets_WhenSellInPositiveAndGreater5() {
-            items = new Item[] {new Item("Backstage passes to a TAFKAL80ETC concert", 6, 30)};
-            testee = new GildedRose(items);
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(0);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(33);
+    }
 
-            testee.updateQuality();
+    @Test
+    void increaseQualityOfTickets_WhenSellInNegative() {
+        items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -1, 30)};
+        testee = new GildedRose(items);
 
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(5);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(32);
-        }
+        testee.updateQuality();
 
-        @Test
-        void increaseQualityOfTickets_WhenSellInPositive() {
-            items = new Item[] {new Item("Backstage passes to a TAFKAL80ETC concert", 1, 30)};
-            testee = new GildedRose(items);
-
-            testee.updateQuality();
-
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(0);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(33);
-        }
-
-        @Test
-        void increaseQualityOfTickets_WhenSellInNegative() {
-            items = new Item[] {new Item("Backstage passes to a TAFKAL80ETC concert", -1, 30)};
-            testee = new GildedRose(items);
-
-            testee.updateQuality();
-
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(-2);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(0);
-        }
-
-
-        @Test
-        void keepQuality_WhenQuality50OrBigger() {
-            items = new Item[] {new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)};
-            testee = new GildedRose(items);
-
-            testee.updateQuality();
-
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(4);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(50);
-        }
-
-        @Test
-        void lowerSellInOnUpdate() {
-            items = new Item[] {
-                new Item("sellInCheck", 10, 10),
-            };
-            testee = new GildedRose(items);
-            testee.updateQuality();
-
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("sellInCheck")).findAny();
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(9);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(9);
-        }
-
-        @Test
-        void lowerQualityOnUpdate() {
-            items = new Item[] {
-                new Item("sellInCheck", 10, 10),
-            };
-            testee = new GildedRose(items);
-            testee.updateQuality();
-
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("sellInCheck")).findAny();
-
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(9);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(9);
-        }
-
-        @Test
-        void lowerQualityOnUpdate_WhenSellInIsNegative() {
-            items = new Item[] {
-                new Item("sellInCheck", -2, 10),
-            };
-            testee = new GildedRose(items);
-            testee.updateQuality();
-
-            Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("sellInCheck")).findAny();
-
-            assertThat(actualCasualSellIn).isPresent();
-            assertThat(actualCasualSellIn.get().sellIn).isEqualTo(-3);
-            assertThat(actualCasualSellIn.get().quality).isEqualTo(8);
-        }
-
-
-
-        @Test
-        void legendaryQualityUpdate() {
-            Item[] items = new Item[] {
-                new Item("Sulfuras, Hand of Ragnaros", 50, 80)
-            };
-            GildedRose testee = new GildedRose(items);
-
-            testee.updateQuality();
-
-            Optional<Item> actualRagnaros = Arrays.stream(testee.items).filter(item -> item.name.equals("Sulfuras, Hand of Ragnaros")).findAny();
-            assertThat(actualRagnaros).isPresent();
-            assertThat(actualRagnaros.get().sellIn).isEqualTo(50);
-            assertThat(actualRagnaros.get().quality).isEqualTo(80);
-        }
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(-2);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(0);
     }
 
 
+    @Test
+    void keepQuality_WhenQuality50OrBigger() {
+        items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)};
+        testee = new GildedRose(items);
+
+        testee.updateQuality();
+
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("Backstage passes to a TAFKAL80ETC concert")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(4);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(50);
+    }
+
+    @Test
+    void lowerSellInOnUpdate() {
+        items = new Item[]{new Item("sellInCheck", 10, 10),};
+        testee = new GildedRose(items);
+        testee.updateQuality();
+
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("sellInCheck")).findAny();
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(9);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(9);
+    }
+
+    @Test
+    void lowerQualityOnUpdate() {
+        items = new Item[]{new Item("sellInCheck", 10, 10),};
+        testee = new GildedRose(items);
+        testee.updateQuality();
+
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("sellInCheck")).findAny();
+
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(9);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(9);
+    }
+
+    @Test
+    void lowerQualityOnUpdate_WhenSellInIsNegative() {
+        items = new Item[]{new Item("sellInCheck", -2, 10),};
+        testee = new GildedRose(items);
+        testee.updateQuality();
+
+        Optional<Item> actualCasualSellIn = Arrays.stream(testee.items).filter(item -> item.name.equals("sellInCheck")).findAny();
+
+        assertThat(actualCasualSellIn).isPresent();
+        assertThat(actualCasualSellIn.get().sellIn).isEqualTo(-3);
+        assertThat(actualCasualSellIn.get().quality).isEqualTo(8);
+    }
+
+
+    @Test
+    void legendaryQualityUpdate() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 50, 80)};
+        GildedRose testee = new GildedRose(items);
+
+        testee.updateQuality();
+
+        Optional<Item> actualRagnaros = Arrays.stream(testee.items).filter(item -> item.name.equals("Sulfuras, Hand of Ragnaros")).findAny();
+        assertThat(actualRagnaros).isPresent();
+        assertThat(actualRagnaros.get().sellIn).isEqualTo(50);
+        assertThat(actualRagnaros.get().quality).isEqualTo(80);
+    }
 }

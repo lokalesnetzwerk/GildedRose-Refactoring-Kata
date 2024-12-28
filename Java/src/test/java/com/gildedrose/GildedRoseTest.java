83b9f2383ab727e449b4c31ab6f766625b7cb1ff
Max <max.joel.mj@googlemail.com>
2024-12-28 13:28:47 +0100

    create acceptancetest

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,32 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void acceptanceTest() {
+        CombinationApprovals.verifyAllCombinations(
+            this::updateItems,
+            new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+            new Integer[]{-1, 0, 1, 4, 6, 7, 11, 12},
+            new Integer[]{0, 1, 49, 50, 51}
+        );
+    }
+
+    private String updateItems(String name, Integer sellIn, Integer quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+
+        GildedRose testee = new GildedRose(items);
+        testee.updateQuality();
+
+        Item actual = items[0];
+        return printItem(actual);
+    }
+
+    private String printItem(Item item) {
+        return "Name: " + item.name + ", SellIn: " + item.sellIn + ", Quality: " + item.quality;
     }
 
 }
